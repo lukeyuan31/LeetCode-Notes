5c0e6bb314ab7708719157eb5c26dd7d5ea03ad0
lukeyuan31 <1393499408@qq.com>
2019-12-19 10:33:34 -0500

    update

---
+++ src/Algorithm/TrappingRainWater.java
@@ -0,0 +1,31 @@
+package Algorithm;
+
+public class TrappingRainWater {
+    public int trap(int[] height) {
+        if(height.length==0){
+            return 0;
+        }
+        int left=0;
+        int right=height.length-1;
+        int leftMax=0;
+        int rightMax=0;
+        int ans = 0;
+        while (left < right){
+            if (height[left]>leftMax){
+                leftMax=height[left];
+            }
+            if (height[right]>rightMax){
+                rightMax=height[right];
+            }
+            if (leftMax<rightMax){
+                ans=ans+leftMax-height[left];
+                left++;
+            }
+            else{
+                ans=ans+rightMax-height[right];
+                right--;
+            }
+        }
+        return ans;
+    }
+}
