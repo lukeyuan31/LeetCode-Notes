81e404d9910e5e1bcbc2d6454c10e5c377d3ded8
lukeyuan31 <1393499408@qq.com>
2019-12-20 14:20:33 -0500

    update

---
+++ src/Algorithm/NumberofIslands.java
@@ -0,0 +1,35 @@
+package Algorithm;
+
+public class NumberofIslands {
+    void dfs(char[][]grid, int x, int y){
+        int length=grid.length;
+        int width=grid[0].length;
+
+        if(x<0 || y <0 || x>=length || y >= width || grid[x][y]=='0'){
+            return;
+        }
+
+        grid[x][y]='0';
+        dfs(grid,x+1,y);
+        dfs(grid,x,y+1);
+        dfs(grid,x-1,y);
+        dfs(grid,x,y-1);
+
+
+    }
+    public int numIslands(char[][] grid) {
+        int result=0;
+        if (grid.length==0 || grid[0].length==0){
+            return 0;
+        }
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]=='1'){
+                    result++;
+                    dfs(grid,i,j);
+                }
+            }
+        }
+        return result;
+    }
+}
