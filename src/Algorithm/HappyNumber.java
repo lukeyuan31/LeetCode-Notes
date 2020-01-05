ead6eb6af3489e83a44a6ba83505f60c85fb6293
lukeyuan31 <1393499408@qq.com>
2020-01-05 11:08:40 -0500

    update

---
+++ src/Algorithm/HappyNumber.java
@@ -0,0 +1,24 @@
+package Algorithm;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HappyNumber {
+    private int getNext(int n){
+        int sum=0;
+        while(n>0){
+            int d = n%10;
+            n=n/10;
+            sum=sum+d*d;
+        }
+        return sum;
+    }
+    public boolean isHappy(int n) {
+        Set<Integer> seen=new HashSet<>();
+        while(n!=1 && !seen.contains(n)){
+            seen.add(n);
+            n=getNext(n);
+        }
+        return n==1;
+    }
+}
