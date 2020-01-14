2a5d6fa19fdc2e561b793704e22020269613668f
lukeyuan31 <1393499408@qq.com>
2020-01-14 14:10:55 -0500

    update

---
+++ src/Algorithm/ReverseSinglyLinkedList.java
@@ -0,0 +1,30 @@
+package Algorithm;
+
+public class ReverseSinglyLinkedList {
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        public ListNode reverseList(ListNode head) {
+            ListNode result=head;
+            ListNode pointer=head;
+            if(head==null){
+                return null;
+            }
+            else{
+                while(pointer.next!=null){
+                    ListNode temp=pointer.next;
+                    pointer.next=pointer.next.next;
+                    temp.next=result;
+                    result=temp;
+                }
+                return result;
+            }
+        }
+    }
+}
