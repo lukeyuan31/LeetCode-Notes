7098172e5654ac23a9314366efe0f2403f4b5da6
Keyuan Lu <11510052@mail.sustc.edu.cn>
2020-03-28 00:21:31 +0800

    update

---
+++ src/Algorithm/Medium/GenerateParentheses.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> result=new ArrayList();
+        generate(result,"",0,0,n);
+        return result;
+    }
+
+    public void generate(List<String> result,String current, int left, int right, int length){
+        if(current.length()==2*length){
+            result.add(current);
+            return;
+        }
+        if(length>left){
+            generate(result,current+"(",left+1,right,length);
+        }
+        if(left>right){
+            generate(result,current+")",left,right+1,length);
+        }
+
+    }
+}
