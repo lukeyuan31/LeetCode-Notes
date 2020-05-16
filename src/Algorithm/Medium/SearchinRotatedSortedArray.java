68d63d3eb716641c4382820f9a32b82dc32e8997
lukeyuan31 <1393499408@qq.com>
2020-05-16 11:47:59 -0400

    update

---
+++ src/Algorithm/Medium/SearchinRotatedSortedArray.java
@@ -0,0 +1,44 @@
+package Algorithm.Medium;
+
+public class SearchinRotatedSortedArray {
+    public int search(int[] nums, int target) {
+        if (nums.length==0){
+            return -1;
+        }
+        if(nums.length==1){
+            if(target==nums[0]){
+                return 0;
+            }
+            else{
+                return -1;
+            }
+        }
+        int head=nums[0];
+        int length=nums.length;
+        int tail=nums[length-1];
+        int counter=0;
+        if(target<head && target>tail){
+            return -1;
+        }
+        if(target>=head){
+            while(counter <length && nums[counter]<=target){
+                if(nums[counter]==target){
+                    return counter;
+                }
+                counter++;
+            }
+            return -1;
+        }
+        if(target<=tail){
+            counter=length-1;
+            while(counter>=0 && nums[counter]>=target){
+                if(nums[counter]==target){
+                    return counter;
+                }
+                counter--;
+            }
+            return -1;
+        }
+        return 1;
+    }
+}
